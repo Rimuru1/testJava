d94c47544903e7c01c50d15200988941107367d3
Arkhane Aimkham <arkhane1126@gmail.com>
2019-08-03 15:50:06 +0700

    WiLoop2

--- WiLoop.java
+++ WiLoop.java
@@ -1,9 +1,15 @@
 public class WiLoop{
  public static void main(String args[]){
          int i=10;
-         while(i>1){
+         while(true){
               System.out.println(i);
               i--;
-         }
+
+                if(i== -100){
+                System.out.println("Stop");
+                break;
+            }
+          
+       }
     }
 }
