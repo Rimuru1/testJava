fe52b0a3d911ed318d1a2d7a04ec347095953ee2
Arkhane Aimkham <arkhane1126@gmail.com>
2019-08-03 11:59:04 +0700

    Test Java4

--- testing.java
+++ testing.java
@@ -2,6 +2,6 @@ public class testing{
     public static void main(String[] args) {
         System.out.println("Hello Wolrd ");
         System.out.println("Hello Wolrd ");
-        System.out.println("Hello Rocker ");
+        System.out.println("Rockerlml ");
     }
 }
